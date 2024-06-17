2bb6d36d86f690c827ef82dc00327609a550f82a
HenrickyL <HenrickyL1@gmail.com>
2024-06-17 11:56:30 -0300

    findOne

--- partners-api/src/events/events.service.ts
+++ partners-api/src/events/events.service.ts
@@ -19,8 +19,10 @@ export class EventsService {
     return this.prismaService.event.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  findOne(id: string) {
+    return this.prismaService.event.findFirst({
+      where: { id: id },
+    });
   }
 
   update(id: number, updateEventDto: UpdateEventDto) {
