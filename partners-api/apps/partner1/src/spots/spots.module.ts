baf88e0042e62a22c209383cab1f9022311d3035
HenrickyL <HenrickyL1@gmail.com>
2024-06-19 09:37:53 -0300

    adjust import

--- partners-api/apps/partner1/src/spots/spots.module.ts
+++ partners-api/apps/partner1/src/spots/spots.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { SpotsService } from './spots.service';
 import { SpotsController } from './spots.controller';
+import { SpotsService } from '@app/core';
 
 @Module({
   controllers: [SpotsController],
