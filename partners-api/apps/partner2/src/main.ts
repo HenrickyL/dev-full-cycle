f4212a6bfd387a12d7867bedabb51bd063ccc0b1
HenrickyL <HenrickyL1@gmail.com>
2024-06-19 09:36:41 -0300

    adjust - add modules in apps

--- partners-api/apps/partner2/src/main.ts
+++ partners-api/apps/partner2/src/main.ts
@@ -5,6 +5,6 @@ import { AllExceptionsFilter } from '_infra/AllExceptionsFilter';
 async function bootstrap() {
   const app = await NestFactory.create(Partner2Module);
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
---
+++ partners-api/apps/partner2/src/spots/dto/SpotStatusEnum.ts
@@ -0,0 +1,4 @@
+export enum SpotStatusEnum {
+  AVAILABLE = 'AVAILABLE',
+  RESERVED = 'RESERVED',
+}
